bd6a6775cfe4e0ef4f6bf6cc74897fe67aeee352
Stefler Gábor <gabor.stefler@gmail.com>
2021-10-22 20:19:25 +0200

    basic static routing works

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const PORT = 3000;
 const express = require('express');
 const app = express();
 
-app.listen(PORT, () => {console.log("Listening on http://127.0.0.1:" + PORT)});
-
+// Statikus fileok kiszolgálására
 app.use(express.static('static'));
+
+// Routing, a routing txt-ben megírtak alapján
+require('./route/routes')(app);
+
+app.listen(PORT, () => {console.log("Listening on http://127.0.0.1:" + PORT)});
